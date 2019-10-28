8378cd6c74f69cdb3fbbf38742bfa779206e3628
GrzegorzKabzinski <grzegorz.kabzinski@htdevelopers.com>
2019-10-28 19:24:49 +0100

    update check string

--- app/controllers/stringapi_controller.rb
+++ app/controllers/stringapi_controller.rb
@@ -1,15 +1,31 @@
 class StringapiController < ApplicationController
+
   def index
-    given_string = params[:mystring]
-    if check_string(given_string)
-      render json: 'A string contains only digits'
-    end
-    if !check_string(given_string)
-      render json: 'A string does not contain digits'
-    end
+    @output_value = 'string contains '
+    given_string = params[:mystring].to_s
+
+    check_string(given_string)
+
+    render json: @output_value
   end
 
-  def check_string(string)
-    string !~ /\D/
+  private
+
+  def check_string(given_string)
+    if given_string.count("0-9") > 0
+      @output_value += 'digits '
+    end
+
+    if given_string.count("A-Z") > 0
+      @output_value += 'capital letters '
+    end
+
+    if given_string.count("a-z") > 0
+      @output_value += 'small letters '
+    end
+
+    if given_string.count('[!@#$%^&*(),.?":{}|<>]') > 0
+      @output_value += 'special signs'
+    end
   end
 end
